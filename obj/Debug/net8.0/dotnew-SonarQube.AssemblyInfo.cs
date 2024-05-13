b3c5c26e19a20b2ed0ba60a408ccde0c9007450c
root <nikg1999@gmail.com>
2024-05-13 12:06:47 +0300

    update name

--- obj/Debug/net8.0/dotnew-SonarQube.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnew-SonarQube.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnew-SonarQube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b106cdc7456413af3245c8ad6a83f47cf6584e6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a4d202c9eadb9ee363ec2e33bb6d95080de0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnew-SonarQube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnew-SonarQube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
